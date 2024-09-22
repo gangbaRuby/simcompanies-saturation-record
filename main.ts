a171cbfbbbdbb2dc088cd717f27714897d847e72
gangbaRuby <40230962+gangbaRuby@users.noreply.github.com>
2024-09-23 00:05:38 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -115,7 +115,7 @@ const htmlContent = `
 
         let showLabels = true; // 初始化数据标签状态
 
-        fetch('https://raw.githubusercontent.com/gangbaRuby/simcompanies-saturation-record/main/data/saturation.json')
+        fetch('https://raw.githubusercontent.com/gangbaRuby/simcompanies-saturation-record/refs/heads/main/data/R1_saturation.json')
             .then(response => response.json())
             .then(data => {
                 // 解析日期
